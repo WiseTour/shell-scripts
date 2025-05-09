320ece07ee0b688b31dbf89bba03a3c3f4cd3f06
KennerLima <kenner.lima@sptech.school>
2025-05-09 16:06:44 -0300

    Update init_setup.sh

--- init_setup.sh
+++ init_setup.sh
@@ -8,15 +8,3 @@ sudo apt install -y docker.io
 
 # Ativa e inicia o Docker
 sudo systemctl enable docker && sudo systemctl start docker
-
-cd scripts_containers/
-
-sudo docker network create wise-network
-
-# Configura os scripts para executaveis
-sudo chmod +x mysql_container_setup.sh
-sudo chmod +x site_container_setup.sh
-
-# Executa os scripts
-./mysql_container_setup.sh
-./site_container_setup.sh
