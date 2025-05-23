0c7acba424000619591c5a6362b7fdfbbcb82ad4
KennerLima <kenner.lima@sptech.school>
2025-05-23 20:03:53 +0000

    nova versão do shell script

--- init_setup.sh
+++ init_setup.sh
@@ -8,3 +8,21 @@ sudo apt install -y docker.io
 
 # Ativa e inicia o Docker
 sudo systemctl enable docker && sudo systemctl start docker
+
+# Instalando docker compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Tornando ele executável
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+REPO_URL="https://github.com/WiseTour/database.git"
+DEST_DIR="mysql/database"
+
+if [ -d "$DEST_DIR/.git" ]; then
+  echo "Repositório já existe. Atualizando com git pull..."
+  cd "$DEST_DIR" && git pull
+else
+  echo "Repositório não encontrado. Clonando com git clone..."
+  git clone "$REPO_URL" "$DEST_DIR"
+fi
